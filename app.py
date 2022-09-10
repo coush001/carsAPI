1edcdc1f8437c97d2452807b64a42b8c7f490810
Hugo <hugo@Hugos-MacBook-Air.local>
2022-09-10 23:22:20 +0100

    add CRUD functionality

--- app.py
+++ app.py
@@ -33,3 +33,51 @@ class CarsModel(db.Model):
 @app.route("/")
 def index():
     return "Hello boss MAN 10!"
+
+
+@app.route('/cars', methods=['POST'])
+def post_car():
+    data = request.get_json()
+    new_car = CarsModel(name=data['name'], model=data['model'], doors=data['doors'], hugo_col=data['hugo_col'])
+    db.session.add(new_car)
+    db.session.commit()
+    return {"message": f"car {new_car.name} has been created successfully."}
+
+
+@app.route('/cars', methods=['GET'])
+def get_cars():
+    cars = CarsModel.query.all()
+    results = [
+        {
+            "name": car.name,
+            "model": car.model,
+            "doors": car.doors
+        } for car in cars]
+    return {"count": len(results), "cars": results}
+
+
+@app.route('/cars/<car_id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_car(car_id):
+    car = CarsModel.query.get_or_404(car_id)
+
+    if request.method == 'GET':
+        response = {
+            "name": car.name,
+            "model": car.model,
+            "doors": car.doors
+        }
+        return {"message": "success", "car": response}
+
+    elif request.method == 'PUT':
+        data = request.get_json()
+        car.name = data['name']
+        car.model = data['model']
+        car.doors = data['doors']
+        db.session.add(car)
+        db.session.commit()
+        return {"message": f"car {car.name} successfully updated"}
+
+    elif request.method == 'DELETE':
+        db.session.delete(car)
+        db.session.commit()
+        return {"message": f"Car {car.name} successfully deleted."}
